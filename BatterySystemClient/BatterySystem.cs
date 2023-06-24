933c75fbbd9a34a58d41505a8290c2635e2995d7
fijiro <jiro.protonmail@protonmail.com>
2023-06-24 22:57:41 +0300

    Draining battery works!

--- BatterySystemClient/BatterySystem.cs
+++ BatterySystemClient/BatterySystem.cs
@@ -1,35 +1,154 @@
-﻿using EFT;
-using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Reflection;
 using Aki.Reflection.Patching;
 using UnityEngine;
 using EFT.InventoryLogic;
 using BatterySystem.Configs;
+using BSG.CameraEffects;
+using Comfort.Common;
+using EFT;
+using EFT.CameraControl;
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using GPUInstancer;
+//TextureMask = FPS Camera
 
 namespace BatterySystem
 {
+
 	public class BatterySystemPatch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(Slot).GetMethod(nameof(Slot.ApplyContainedItem));
+		}
+
+		public static bool drainBattery = false;
+		public static Slot batterySlot = null;
+
+		[PatchPostfix]
+		static void Postfix(ref Slot __instance)
+		{
+			if (!BatterySystemConfig.EnableMod.Value || Singleton<GameWorld>.Instance == null) return;
+			if (__instance.ContainedItem.TemplateId != "aaa-battery") return;
+			batterySlot = __instance;
+			NightVision nightVision = GameObject.Find("FPS Camera").GetComponent<NightVision>();
+			
+			Logger.LogInfo("--- BATTERYSYSTEM ---");
+			Logger.LogInfo("Parent: " + __instance.ParentItem);
+			Logger.LogInfo("ParentP: " + __instance.ParentItem.Parent);
+			Logger.LogInfo("Container: " + __instance.ParentItem.Parent.Container.ToString());
+			Logger.LogInfo("ContainerName: " + __instance.ParentItem.Parent.ContainerName);
+
+			if (__instance.Items.Count() == 0) // item being added
+			{
+				Logger.LogInfo("Contains 0: " + __instance.ContainedItem);
+				drainBattery = false; //stop burning resource
+				
+				if (__instance.ParentItem.TemplateId == "5c0558060db834001b735271" || __instance.ParentItem.TemplateId == "5c066e3a0db834001b7353f0") //pgnvg-avnis || armasight n-15 + nvg
+				{
+					//disable nvg
+					Logger.LogInfo("Removing item from NVGs!");
+
+				}
+				else Logger.LogInfo("ParentItem.TemplateId Unknown: " + __instance.ParentItem.TemplateId);
+			}
+			else if (__instance.Items.Count() == 1) // item being added
+			{
+				Logger.LogInfo("Contains 1: " + __instance.ContainedItem);
+
+				drainBattery = true;//start burning resource
+				//GClass1810 burnResource = new GClass1810();
+				//GClass1812 burnables = new GClass1812();
+				//GClass2184 gClass2184 = new GClass2184();
+				//Logger.LogInfo(gClass2184.CompatibleItems.ToString());
+				//GClass2361 gClass2361 = new GClass2361(__instance.ContainedItem.TemplateId, gClass2184);
+				//burnResource.SetItems(gClass2361[]);
+				//if (burnResource.SupplyItems[0].TemplateId == "") { }
+				if (__instance.ParentItem.TemplateId == "5c0558060db834001b735271" || __instance.ParentItem.TemplateId == "5c066e3a0db834001b7353f0") //pgnvg-anvis || armasight n-15 + nvg
+				{
+					//enable nvg
+					Logger.LogInfo("Adding item to NVGs!");
+				}
+			}
+
+			//Player player = Singleton<GameWorld>.Instance.MainPlayer;
+			//InventoryControllerClass inventoryController = (InventoryControllerClass)AccessTools.Field(typeof(Player), "_inventoryController").GetValue(player);
+			//Item helmet = inventoryController.Inventory.Equipment.GetSlot(EquipmentSlot.Headwear).FindItem("aaa-battery");
+			//Logger.LogInfo(helmet);
+			nightVision.ApplySettings();
+		}
+	}
+	public class NightVisionPatch : ModulePatch
+	{
+
+		private static Color temp = Color.black;
+		public static NightVision nightVision;
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(NightVision).GetMethod(nameof(NightVision.ApplySettings));
+		}
+
+		[PatchPostfix]
+		static void Postfix(ref NightVision __instance)
+		{
+			nightVision = __instance;
+			//if(__instance.name != "FPS Camera") return;
+			Logger.LogInfo("--- BATTERYSYSTEM ---");
+			Logger.LogInfo("Nvg apply settings: " + __instance);
+			Logger.LogInfo($"At: {Time.time}s");
+			if (BatterySystemPlugin.gameWorld == null || BatterySystemPlugin.gameWorld.MainPlayer == null) { }
+			else if (BatterySystemPatch.drainBattery == true) //enable nvg
+			{
+				__instance.Color.r = 0.1f;
+				__instance.Color.g = 1;
+				__instance.Color.b = 0.1f;
+			}
+			else if (BatterySystemPatch.drainBattery == false) //disable
+			{
+				temp = __instance.Color;
+				__instance.Color = Color.black;
+
+			}
+		}
+	}
+
+	public class NightVisionSwitchPatch : ModulePatch
 	{
 
 		protected override MethodBase GetTargetMethod()
 		{
-			return typeof(Slot).GetMethod(nameof(Slot.Add));
+			return typeof(NightVision).GetMethod(nameof(NightVision.StartSwitch)); // Startswitch(BOOL), false = off, true = on
 		}
 
 		[PatchPostfix]
-		static void Postfix(ref Slot __instance, Item item)
+		static void Postfix(ref NightVision __instance, bool on)
 		{
-			if (!BatterySystemConfig.EnableMod.Value) return;
+			//__instance.enabled = false disables StartSwitch, but animation still happens
+			Logger.LogInfo("On: " + on + ", switching at: " + Time.time);
+			//set color to black for the switch duration
+		}
+	}
+	public class ForceSwitchPatch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(PlayerCameraController).GetMethod("method_4", BindingFlags.Instance | BindingFlags.NonPublic); // nvg toggle button pressed
+		}
 
-			if (item.TemplateId == "aaa-battery" && (__instance.ParentItem.TemplateId == "5d1b5e94d7ad1a2b865a96b0" || __instance.ParentItem.TemplateId == "5c0558060db834001b735271")) //flir+gpnvg
+		[PatchPostfix]
+		static void Postfix(ref PlayerCameraController __instance)
+		{
+			NightVision nightVision = __instance.GetComponent<NightVision>();
+			Logger.LogInfo("--- BATTERYSYSTEM ---");
+			Logger.LogInfo("Method4 at" + Time.time);
+			if (__instance.Camera.name == "FPS Camera")
 			{
-				Logger.LogInfo("--- BATTERYSYSTEM ---");
-				Logger.LogInfo("Item: " + item.TemplateId);
-				Logger.LogInfo($"At: {Time.time}");
-				Logger.LogInfo($"To slot: {__instance.Name}");
-				Logger.LogInfo($"Parent: {__instance.ParentItem.TemplateId}");
-
+				Logger.LogInfo("passed");
+				// call nvg-slot.applyitems();
+				//nightVision.StartSwitch(true);
+				//nightVision.enabled = false;
 			}
 		}
 	}
--- BatterySystemClient/Plugin.cs
+++ BatterySystemClient/Plugin.cs
@@ -3,9 +3,11 @@ using BatterySystem.Configs;
 using EFT;
 using EFT.InventoryLogic;
 using Comfort.Common;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using System.ComponentModel;
+using System.Collections.Generic;
+using HarmonyLib;
+using System.Collections;
+using System;
 
 namespace BatterySystem
 {
@@ -13,29 +15,98 @@ namespace BatterySystem
 	[BepInPlugin("com.jiro.batterysystem", "BatterySystem", "1.0.0")]
 	public class BatterySystemPlugin : BaseUnityPlugin
 	{
+		public static GameWorld gameWorld;
+		public static Player gamePlayer;
+		private static Item battery;
+		private static int cooldown = 5;
+
 		void Awake()
 		{
 			BatterySystemConfig.Init(Config);
 			new BatterySystemPatch().Enable();
-
+			new NightVisionPatch().Enable();
+			new ForceSwitchPatch().Enable();
+			new NightVisionSwitchPatch().Enable();
 		}
-/*
+
 		void Update()
 		{
 			gameWorld = Singleton<GameWorld>.Instance;
-			var gamePlayer = gameWorld.AllPlayers[0];
-			//gamePlayer.TryGetItemInHands<;
-			var primary = EquipmentSlot.FirstPrimaryWeapon;
-			var secondary = EquipmentSlot.SecondPrimaryWeapon;
-			var holster = EquipmentSlot.Holster;
-			var headwear = EquipmentSlot.Headwear;
-			InventoryControllerClass inventoryControllerClass = new InventoryControllerClass(gamePlayer.Profile, true);
-			var itemInHands = inventoryControllerClass.ItemInHands;
-			//sheesh
-			var slot = Singleton<Slot>.Instance.ContainedItem;
-			//slot;
-			player = GameObject.Find("PlayerSuperior(clone)");
+			if (gameWorld == null || gameWorld.MainPlayer == null) return;
+			//if (gameWorld == null || gameWorld.MainPlayer == null) return;
+			if (Time.time > cooldown)
+			{
+				cooldown = (int)Time.time + 1;
 
-		}*/
+				if (BatterySystemPatch.drainBattery)
+				{
+					//BatterySystemPatch.batterySlot.ApplyContainedItem();
+					//ResourceComponent resource = (ResourceComponent)AccessTools.Field(typeof(ResourceComponent), "Resource").GetValue(BatterySystemPatch.batterySlot.ContainedItem);
+					 ResourceComponent resourceComponent = BatterySystemPatch.batterySlot.ContainedItem.GetItemComponent<ResourceComponent>();
+
+					if (resourceComponent != null)
+					{
+
+						resourceComponent.Value -= 5;
+						Logger.LogInfo(resourceComponent);
+						Logger.LogInfo(resourceComponent.Value);
+						Logger.LogInfo(resourceComponent.MaxResource);
+					}
+					}
+					//Logger.LogInfo(gameWorld.MainPlayer.NightVisionObserver.GetItemComponent().Item);
+					// EFT.InventoryLogic.NightVisionComponent
+				}
+				//gamePlayer = gameWorld.MainPlayer;
+				//if (gamePlayer == null) return;
+
+				//Item itemInHands = inventoryControllerClass.ItemInHands;
+				//battery = BatterySystemPatch.batterySlot.ContainedItem;
+				//List<string> equippedTpl = inventoryControllerClass.Inventory.EquippedInSlotsTemplateIds;
+				//inventoryControllerClass.ContainedItems;
+
+
+			}
+			/*
+			private static IEnumerator LowerThermalBattery(Player player)
+			{
+				if (player == null)
+				{
+					yield break;
+				}
+
+				while (player.HealthController != null && player.HealthController.IsAlive)
+				{
+					yield return null;
+					ThermalVisionComponent thermalVisionComponent = player.ThermalVisionObserver.GetItemComponent();
+					if (thermalVisionComponent == null)
+					{
+						continue;
+					}
+
+					if (thermalVisionComponent.Togglable.On)
+					{
+						IEnumerable<ResourceComponent> resourceComponents = thermalVisionComponent.Item.GetItemComponentsInChildren<ResourceComponent>(false);
+						foreach (ResourceComponent resourceComponent in resourceComponents)
+						{
+							if (resourceComponent == null)
+							{
+								thermalVisionComponent.Togglable.Set(false);
+								continue;
+							}
+
+							Single targetValue = resourceComponent.Value - Instance.BatteryDrainRate.Value * Time.deltaTime;
+							if (targetValue <= 0f)
+							{
+								targetValue = 0f;
+							}
+
+							if ((resourceComponent.Value = targetValue).IsZero())
+							{
+								thermalVisionComponent.Togglable.Set(false);
+							}
+						}
+					}
+				}*/
+		}
 	}
 }

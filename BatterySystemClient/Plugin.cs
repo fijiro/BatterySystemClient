5bc43d581169ec72fe663de089868778ef000e81
fijiro <jiro.protonmail@protonmail.com>
2023-06-25 20:14:31 +0300

    add item multiplier

--- BatterySystemClient/Plugin.cs
+++ BatterySystemClient/Plugin.cs
@@ -6,6 +6,7 @@ using Comfort.Common;
 using UnityEngine;
 using System.Collections.Generic;
 using HarmonyLib;
+using System.Collections;
 
 namespace BatterySystem
 {
@@ -16,7 +17,7 @@ namespace BatterySystem
 		public static GameWorld gameWorld;
 		private static float cooldown = 5;
 		public static Dictionary<string, Color> nvgDefaultColor = new Dictionary<string, Color>();
-
+		public static Dictionary<string, float> itemDrainMultiplier = new Dictionary<string, float>();
 		void Awake()
 		{
 			BatterySystemConfig.Init(Config);
@@ -27,15 +28,21 @@ namespace BatterySystem
 			//foreach (ItemTemplate template in ItemTemplates)
 			{
 				nvgDefaultColor.Add("5c0696830db834001d23f5da", new Color(0, 255 / 255f, 32 / 255f, 254)); // PNV-10T Night Vision Goggles
+				itemDrainMultiplier.Add("5c0696830db834001d23f5da", 1f);
+
 				nvgDefaultColor.Add("5c0558060db834001b735271", new Color(83 / 255f, 255 / 255f, 69 / 255f, 254)); // GPNVG-18 Night Vision goggles
+				itemDrainMultiplier.Add("5c0696830db834001d23f5da", 2f);
 				nvgDefaultColor.Add("5c066e3a0db834001b7353f0", new Color(0, 255 / 255f, 243 / 255f, 254)); // Armasight N-15 Night Vision Goggles
+				itemDrainMultiplier.Add("5c066e3a0db834001b7353f0", 1f);
+
 				nvgDefaultColor.Add("57235b6f24597759bf5a30f1", new Color(183 / 255f, 255 / 255f, 86 / 255f, 254)); // AN/PVS-14 Night Vision Monocular
+				itemDrainMultiplier.Add("57235b6f24597759bf5a30f1", 0.5f);
 
 				//nvgDefaultColor.Add("5c110624d174af029e69734c", new Color(0, 255, 32)); // T-7 Thermal Goggles with a Night Vision mount
 			}
 		}
 		void Update()
-		{
+		{ 
 			if (!BatterySystemConfig.EnableMod.Value) return;
 
 			gameWorld = Singleton<GameWorld>.Instance;
@@ -47,7 +54,7 @@ namespace BatterySystem
 				if (resourceComponent.Value > 0 && NightVisionPatch.nightVision.On)
 				{
 					cooldown = (int)Time.time + 5;
-					resourceComponent.Value -= 1 / 7.2f * BatterySystemConfig.DrainMultiplier.Value; //Default battery lasts 1 hr
+					resourceComponent.Value -= 1 / 7.2f * BatterySystemConfig.DrainMultiplier.Value * itemDrainMultiplier[BatterySystemPatch.batterySlot.ParentItem.TemplateId]; //Default battery lasts 1 hr * configmulti * itemmulti
 					BatterySystemPatch.batterySlot.ApplyContainedItem();
 				}
 
